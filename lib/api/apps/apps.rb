87ddbdc3c3bca496cf7871cf3358e86a38d2d0ba
Guy Moore <guy.moore.normal@gmail.com>
2017-10-09 11:22:49 -0700

    Add apps methods with client

--- lib/api/apps/apps.rb
+++ lib/api/apps/apps.rb
@@ -1,13 +1,30 @@
 class Apps
-	def list()
+	def initialize(client)
+		@client = client
+		@ENDPOINT = client.ENDPOINT + "/" + "apps"
 	end
 
-	def delete()
+	def list()
+		method = "GET"
+		return self.client.request(method, self.ENDPOINT)
+	end
+
+	def delete(appid)
+		method = "DELETE"
+		if appid.is_a? Integer
+			raise "Non integer appid"
+		end
+		endpoint = self.ENDPOINT + "/" + appid
+		return self.client.request(method, endpoint)
 	end
 
 	def delete_branding()
+		method = "DELETE"
+		return self.client.request(method, self.ENDPOINT)
 	end
 
 	def create()
+		method = "POST"
+		return self.client.request(method, self.ENDPOINT)
 	end
 end
