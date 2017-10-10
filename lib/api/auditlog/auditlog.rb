d358c7dfe01170b266db16eeb842da5f92faa3e3
Guy Moore <guy.moore.normal@gmail.com>
2017-10-09 22:26:00 -0700

    Don't change the endpoint per class, change it per method

--- lib/api/auditlog/auditlog.rb
+++ lib/api/auditlog/auditlog.rb
@@ -1,11 +1,10 @@
 class AuditLog < ApiModule
 	def initialize(client)
 		super(client)
-		@ENDPOINT = client.ENDPOINT << "/auditlogtoken"
 	end
 
 	def get_token(targetid)
-		endpoint = self.ENDPOINT << "?target_id=" << targetid
+		endpoint = client.ENDPOINT << "?target_id=" << targetid
 		return self.client.request("GET", endpoint)
 	end
 end
