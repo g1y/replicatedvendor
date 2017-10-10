817478fab73427d5f6ef5355e31728b54eb96579
Daryl Metzler <daryl@ifixit.com>
2017-10-09 23:04:38 -0700

    Fix tabs to spaces
    and restore history.

--- lib/client/client.rb
+++ lib/client/client.rb
@@ -4,44 +4,44 @@ require "ostruct"
 require "json"
 
 class ApiClient
-   VENDOR_HOST = "api.replicated.com"
-   VERB_MAP = {
-      :get    => Net::HTTP::Get,
-      :post   => Net::HTTP::Post,
-      :put    => Net::HTTP::Put,
-      :delete => Net::HTTP::Delete
-   }
+	VENDOR_HOST = "api.replicated.com"
+	VERB_MAP = {
+		:get	 => Net::HTTP::Get,
+		:post	=> Net::HTTP::Post,
+		:put	 => Net::HTTP::Put,
+		:delete => Net::HTTP::Delete
+	}
 
-   def initialize
-      # Create persistent HTTP connection
-      @http = Net::HTTP.new(VENDOR_HOST, URI::HTTPS::DEFAULT_PORT,
-       :use_ssl => uri.scheme == 'https')
-   end
+	def initialize
+		# Create persistent HTTP connection
+		@http = Net::HTTP.new(VENDOR_HOST, URI::HTTPS::DEFAULT_PORT,
+		 :use_ssl => uri.scheme == 'https')
+	end
 
-   def set_token(api_token)
-      @api_token = api_token
-   end
+	def set_token(api_token)
+		@api_token = api_token
+	end
 
-   def request_json(method, uri, params = nil)
-      response = request(method, uri, params)
-      body = JSON.parse(response.body)
+	def request_json(method, uri, params = nil)
+		response = request(method, uri, params)
+		body = JSON.parse(response.body)
 
-      OpenStruct.new(:code => response.code, :body => body)
-   rescue JSON::ParserError
-      response
-   end
+		OpenStruct.new(:code => response.code, :body => body)
+	rescue JSON::ParserError
+		response
+	end
 
-   def request(method, uri, params)
-      method_sym = method.downcase.to_sym
+	def request(method, uri, params)
+		method_sym = method.downcase.to_sym
 
-      unless method_sym.is_eql? :get
-         request.set_form_data(params)
-      end
+		unless method_sym.is_eql? :get
+			request.set_form_data(params)
+		end
 
-      if @api_token
-         request['Authorization'] = @api_token
-      end
+		if @api_token
+			request['Authorization'] = @api_token
+		end
 
-      @http.request(request)
-   end
+		@http.request(request)
+	end
 end
