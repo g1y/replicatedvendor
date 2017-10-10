ae476e869f528735735e6e04b1d5004707da192e
Daryl Metzler <daryl@ifixit.com>
2017-10-09 20:07:39 -0700

    Add empty module entrypoints

---
+++ lib/api.rb
@@ -0,0 +1,52 @@
+require_relative "./client/client.rb"
+
+class VendorApi
+   def initialize(api_token = nil)
+      @api_token = api_token
+      @client = ApiClient.new()
+   end
+
+   def app
+
+   end
+   
+   def apps
+
+   end
+
+   def auditlog
+
+   end
+
+   def auditlog
+
+   end
+
+   def auth
+
+   end
+
+   def channel
+
+   end
+
+   def license
+
+   end
+
+   def licenes
+
+   end
+
+   def release
+
+   end
+
+   def releases
+
+   end
+
+   def team
+
+   end
+end
