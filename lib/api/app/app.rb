e47962b7d834b0b80f80c149d904d99794aec2b6
Daryl Metzler <daryl@ifixit.com>
2017-10-09 23:55:24 -0700

    use uri not endpoint

--- lib/api/app/app.rb
+++ lib/api/app/app.rb
@@ -14,41 +14,41 @@ class App < ApiModule
 		method = "POST"
 		endpoint = "app/" + appid + "/branding"
 		uri = ApiUri::build_uri(endpoint)
-		return self.client.request(method, endpoint)
+		return self.client.request(method, uri)
 	end
 
 	def list_license_fields(appid:)
 		method = "GET"
 		endpoint = "app/" + appid + "/licensefield"
 		uri = ApiUri::build_uri(endpoint)
-		return self.client.request(method, endpoint)
+		return self.client.request(method, uri)
 	end
 
 	def create_license_field(appid:, options:)
 		method = "POST"
 		endpoint = "app/" + appid + "/licensefield"
 		uri = ApiUri::build_uri(endpoint)
-		return self.client.request(method, endpoint, options)
+		return self.client.request(method, uri, options)
 	end
 
 	def edit_license_field(appid:, license_field_name:, options:)
 		method = "PUT"
 		endpoint = "app/" + appid + "/licensefield/" + license_field_name
 		uri = ApiUri::build_uri(endpoint)
-		return self.client.request(method, endpoint, options)
+		return self.client.request(method, uri, options)
 	end
 
 	def delete_license_field(appid:, license_field_name:)
 		method = "DELETE"
 		endpoint = "app/" + appid + "/licensefield/" + license_field_name
 		uri = ApiUri::build_uri(endpoint)
-		return self.client.request(method, endpoint)
+		return self.client.request(method, uri)
 	end
 
 	def list_license(appid:)
 		method = "GET"
 		endpoint = "app/" + appid + "/licenses"
 		uri = ApiUri::build_uri(endpoint)
-		return self.client.request(method, endpoint)
+		return self.client.request(method, uri)
 	end
 end
