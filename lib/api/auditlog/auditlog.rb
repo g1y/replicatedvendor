9803639cf6d82b54d6d2ffde2b1e66f79a552eda
Guy Moore <guy.moore.normal@gmail.com>
2017-10-09 22:24:05 -0700

    Use shovels

--- lib/api/auditlog/auditlog.rb
+++ lib/api/auditlog/auditlog.rb
@@ -1,11 +1,11 @@
 class AuditLog < ApiModule
 	def initialize(client)
 		super(client)
-		@ENDPOINT = client.ENDPOINT + "/auditlogtoken"
+		@ENDPOINT = client.ENDPOINT << "/auditlogtoken"
 	end
 
 	def get_token(targetid)
-		endpoint = self.ENDPOINT + "?target_id=" + targetid
+		endpoint = self.ENDPOINT << "?target_id=" << targetid
 		return self.client.request("GET", endpoint)
 	end
 end
