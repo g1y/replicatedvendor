0ec8b1ef58e0e302b710bf92933c82e059a34d22
Daryl Metzler <daryl@ifixit.com>
2017-10-09 20:39:42 -0700

    Tabs for spaces

--- lib/api/apimodule.rb
+++ lib/api/apimodule.rb
@@ -1,7 +1,7 @@
 Dir[File.dirname(__FILE__) + '/**/*.rb'].each {|file| require file }
 
 class ApiModule
-   def initialize(client = nil)
-      @client = client
-   end
+	def initialize(client = nil)
+		@client = client
+	end
 end
