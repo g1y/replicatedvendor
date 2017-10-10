736419347d8d835a61955863ba9179a736be3699
Daryl Metzler <daryl@ifixit.com>
2017-10-09 23:28:09 -0700

    Use uri in App

--- lib/api/app/app.rb
+++ lib/api/app/app.rb
@@ -4,23 +4,31 @@ class App < ApiModule
 	end
 
 	def list_branding(appid)
+		method = "GET"
 		endpoint = "app/" + appid + "/branding"
-		return self.client.request("GET", endpoint)
+      uri = ApiUri::build_uri(endpoint)
+		return self.client.request(method, uri)
 	end
 
 	def create_branding(appid:)
+		method = "POST"
 		endpoint = "app/" + appid + "/branding"
-		return self.client.request("POST", endpoint)
+      uri = ApiUri::build_uri(endpoint)
+		return self.client.request(method, endpoint)
 	end
 
 	def list_license_fields(appid:)
+		method = "GET"
 		endpoint = "app/" + appid + "/licensefield"
-		return self.client.request("GET", endpoint)
+      uri = ApiUri::build_uri(endpoint)
+		return self.client.request(method, endpoint)
 	end
 
 	def create_license_field(appid:, default:, hidden:, name:, required:, title:, type:)
+		method = "POST"
 		endpoint = "app/" + appid + "/licensefield"
-		return self.client.request("POST", endpoint, {
+      uri = ApiUri::build_uri(endpoint)
+		return self.client.request(method, endpoint, {
 			"default": default,
 			"hidden": hidden,
 			"name": name,
@@ -31,8 +39,10 @@ class App < ApiModule
 	end
 
 	def edit_license_field(appid, license_field_name, default, hidden, title)
+		method = "PUT"
 		endpoint = "app/" + appid + "/licensefield/" + license_field_name
-		return self.client.request("PUT", endpoint, {
+      uri = ApiUri::build_uri(endpoint)
+		return self.client.request(method, uri, {
 			"default": default,
 			"hidden": hidden,
 			"title": title,
@@ -40,12 +50,16 @@ class App < ApiModule
 	end
 
 	def delete_license_field(appid, license_field_name)
+		method = "DELETE"
 		endpoint = "app/" + appid + "/licensefield/" + license_field_name
-		return self.client.request("DELETE", endpoint)
+      uri = ApiUri::build_uri(endpoint)
+		return self.client.request(method, endpoint)
 	end
 
 	def list_license(appid)
+		method = "GET"
 		endpoint = "app/" + appid + "/licenses"
-		return self.client.request("GET", endpoint)
+      uri = ApiUri::build_uri(endpoint)
+		return self.client.request(method, endpoint)
 	end
 end
