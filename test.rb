0995e2a0bc9da7beb6656aea5a7cd7facd6054e1
Daryl Metzler <daryl@ifixit.com>
2017-10-12 13:57:39 -0700

    Pick up API token from ENV
    And abort if it is not set.

--- test.rb
+++ test.rb
@@ -1,3 +1,10 @@
-require "api"
+require "./lib/api.rb"
 
-api = VendorApi.new
+api_token = ENV['REPLICATED_API_TOKEN']
+app_id = ENV['REPLICATED_APP_ID']
+
+unless api_token
+   abort("Please set REPLICATED_API_TOKEN in your environment")
+end
+
+api = VendorApi.new(api_token)
