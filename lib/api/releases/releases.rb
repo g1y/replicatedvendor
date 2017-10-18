6c2a116f89c0593a8cce2de190737cb3667427e1
Daryl Metzler <daryl@ifixit.com>
2017-10-17 22:46:02 -0700

    Remove colons from releases

--- lib/api/releases/releases.rb
+++ lib/api/releases/releases.rb
@@ -1,12 +1,12 @@
 class Releases < ApiModule
-	def list_releases(app_id:)
+	def list_releases(app_id)
 		method = "GET"
 		endpoint = "app/" + app_id + "/releases"
 		uri = ApiUri::build_uri(endpoint)
 		return @client.request_json(method, uri)
 	end
 
-	def get_releases(app_id:, options:)
+	def get_releases(app_id, options)
 		method = "GET"
 		endpoint = "app/" + app_id + "/releases/paged"
 		uri = ApiUri::build_uri(endpoint, options)
