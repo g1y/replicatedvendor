52f1db5ec08632f218f8468eb9b155674310012e
Guy Moore <guy.moore.normal@gmail.com>
2017-10-17 22:48:53 -0700

    Remove required parameters
    It's really annoying to have to specify the name.

--- lib/api/apps/apps.rb
+++ lib/api/apps/apps.rb
@@ -10,21 +10,21 @@ class Apps < ApiModule
 		return @client.request_json(method, uri)
 	end
 
-	def delete(app_id:)
+	def delete(app_id)
 		method = "DELETE"
 		endpoint = "app/" + app_id
 		uri = ApiUri::build_uri(endpoint)
 		return @client.request_json(method, uri)
 	end
 
-	def delete_branding(app_id:)
+	def delete_branding(app_id)
 		method = "DELETE"
 		endpoint = "app/" + app_id + "/branding"
 		uri = ApiUri::build_uri(endpoint)
 		return @client.request_json(method, uri)
 	end
 
-	def create(options:)
+	def create(options)
 		method = "POST"
 		endpoint = "app"
 		uri = ApiUri::build_uri(endpoint)
--- lib/api/channel/channel.rb
+++ lib/api/channel/channel.rb
@@ -1,33 +1,33 @@
 class Channel < ApiModule
-	def create_channel(app_id:, options:)
+	def create_channel(app_id, options)
 		method = "POST"
 		endpoint = "app/" + app_id + "/channel"
 		uri = ApiUri::build_uri(endpoint)
 		return @client.request_json(method, uri, options)
 	end
 
-	def update_channel(app_id:, channel_id:, options:)
+	def update_channel(app_id, channel_id, options)
 		method = "POST"
 		endpoint = "app/" + app_id + "/channel/" + channel_id
 		uri = ApiUri::build_uri(endpoint)
 		return @client.request_json(method, uri, options)
 	end
 
-	def archive_channel(app_id:, channel_id:)
+	def archive_channel(app_id, channel_id)
 		method = "POST"
 		endpoint = "app/" + app_id + "/channel/" + channel_id + "/archive"
 		uri = ApiUri::build_uri(endpoint)
 		return @client.request_json(method, uri)
 	end
 
-	def update_release(app_id:, channel_id:, sequence:, options:)
+	def update_release(app_id, channel_id, sequence, options)
 		method = "POST"
 		endpoint = "app/" + app_id + "/channel/" + channel_id + "/release/" + sequence
 		uri = ApiUri::build_uri(endpoint)
 		return @client.request_json(method, uri, options)
 	end
 
-	def list_releases(app_id:, channel_id:)
+	def list_releases(app_id, channel_id)
 		method = "GET"
 		endpoint = "app/" + app_id + "/channel/" + channel_id + "/releases"
 		uri = ApiUri::build_uri(endpoint)
